90bbcfa751a22beb5495024ce26168b701e24cdc
YuriMel0 <meloy316@gmail.com>
2024-09-16 20:29:22 -0300

    adicionando conexão com banco de dados, corrigindo bug no ticker

--- src/extract_load.py
+++ src/extract_load.py
@@ -6,15 +6,35 @@ import os
 
 commodities = ['CL=F', 'GC=F', 'SI=F']
 
+load_dotenv()
+
+DB_HOST = os.getenv("DB_HOST_PROD")
+DB_PORT = os.getenv("DB_PORT_PROD")
+DB_NAME = os.getenv("DB_NAME_PROD")
+DB_USER = os.getenv("DB_USER_PROD")
+DB_PASS = os.getenv("DB_PASS_PROD")
+DB_SCHEMA = os.getenv("DB_SCHEMA_PROD")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+
 def buscar_dados_commodities(simbolo, periodo='5y', intervalos='1d'):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(simbolo)
     dados = ticker.history(period=periodo, interval=intervalos)[['Close']]
     dados['simbolo'] = simbolo
     return dados
 
-def agrupar_dados_commodities(comodities):
+def agrupar_dados_commodities(commodities):
     todos_dados = []
     for simbolo in commodities:
         dados = buscar_dados_commodities(simbolo)
         todos_dados.append(dados)
     return pd.concat(todos_dados)
+
+def salvar_banco_dados(df, schema="public"):
+    df.to_sql('commodities', engine, if_exists='replace', index=True, index_label='Date', schema=schema)
+
+if __name__ == "__main__":
+    dados_concatenados = agrupar_dados_commodities(commodities)
+    salvar_banco_dados(dados_concatenados, schema="public")
